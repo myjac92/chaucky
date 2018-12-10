96f6cd2f4e94fc26a9d0e5f4eea9f6cb63fe825d
josé Antônio <myjac92@gmail.com>
2018-12-10 15:11:56 -0200

    detalhes do protuto

--- site.php
+++ site.php
@@ -1,39 +1,50 @@
 <?php
 use \Hcode\Page;
-//use \Hcode\PageAdmin;
-//use \Hcode\Model\User;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
 
 $app->get('/', function() {
 
-      $products = Product::listAll();
-      $page = new Page();
-      $page->setTpl("index",[
-        "products"=>Product::checkList($products)
-      ]);
+$products = Product::listAll();
+$page = new Page();
+$page->setTpl('index',[
+  'products'=>Product::checkList($products)
+]);
 });
-$app->get("/categories/:idcategory", function($idcategory){
+
+$app -> get("/categories/:idcategory",function($idcategory){
 
   $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
   $category = new Category();
   $category->get((int)$idcategory);
-  $pagination= $category->getProducstPage($page);
-  $pages=[];
-  for ($i=1; $i <=$pagination['pages'] ; $i++) {
+  $pagination = $category->getProducstPage($page);
+  $pages = [];
+  for ($i=1; $i<=$pagination['pages'] ; $i++) {
     array_push($pages,[
       'link'=>'/categories/'.$category->getidcategory().'?page='.$i,
       'page'=>$i
     ]);
+
   }
   $page = new Page();
   $page->setTpl("category",[
-  'category'=>$category->getValues(),
-  'products'=>$pagination["data"],
-  'pages'=>$pages
+    'category'=>$category->getValues(),
+    'products'=>$pagination["data"],
+    'pages'=>$pages
   ]);
 });
+$app->get("/products/:desurl",function($desurl){
+
+  $product = new Product();
+  $product->getFromURL($desurl);
+  $page = new Page();
+  $page ->setTpl("product-detail",[
+    'product'=>$product->getValues(),
+    'Categories'=>$product->getCategories()
+  ]);
+});
+
 
 
  ?>
