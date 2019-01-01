3f1a21e1f4f0f7700379e8291ddcb0081438a37e
josé Antônio <myjac92@gmail.com>
2018-12-31 23:36:49 -0200

    admin-user- search and pagination

--- admin-users.php
+++ admin-users.php
@@ -7,10 +7,32 @@ use \Hcode\Model\User;
 $app ->get("/admin/users",function(){
 
       User::verifyLogin();
-      $users = User::listAll();
+      $search = (isset($_GET['search'])) ? $_GET['search'] : "";
+      $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+      if ($search != '') {
+          $pagination = User::getPageSearch($search,$page);
+      }else {
+          $pagination = User::getPage($page);
+      }
+    //  $users = User::listAll();
+
+      $pages =[];
+
+      for ($x = 0; $x <$pagination['pages'] ; $x++) {
+        array_push($pages,[
+          'href'=>'/admin/users?'.http_build_query([
+            'page'=>$x+1,
+            'search'=>$search
+          ]),
+          'text'=>$x+1
+        ]);
+      }
       $page = new PageAdmin();
       $page->setTpl("users",[
-        'users'=>$users
+        'users'=>$pagination['data'],
+        'search'=>$search,
+        'pages'=>$pages
       ]);
 });
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -333,6 +333,49 @@ public function getOrders(){
    return $results;
 
 }
+public static function getPage($page=1,$itemPerPage = 10)
+{
+  $start = ($page - 1 ) * $itemPerPage;
+  $sql = new Sql();
+  $results=$sql->select("SELECT SQL_CALC_FOUND_ROWS *
+          FROM tb_users a
+          INNER JOIN tb_persons b USING(idperson)
+          ORDER BY b.desperson
+          LIMIT $start,$itemPerPage;
+          ");
+  $resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+  return [
+
+    'data'=>$results,
+    'total'=>(int)$resultTotal[0]["nrtotal"],
+    'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemPerPage)
+  ];
+}
+
+public static function getPageSearch($search,$page=1,$itemPerPage = 10)
+{
+  $start = ($page - 1 ) * $itemPerPage;
+  $sql = new Sql();
+  $results=$sql->select("SELECT SQL_CALC_FOUND_ROWS *
+          FROM tb_users a
+          INNER JOIN tb_persons b USING(idperson)
+          WHERE b.desperson LIKE :search OR b.desemail = :search OR a.deslogin LIKE :search
+          ORDER BY b.desperson
+          LIMIT $start,$itemPerPage;
+          ",[
+            ':search'=>'%'.$search.'%'
+          ]);
+  $resultTotal = $sql->select("SELECT FOUND_ROWS() AS nrtotal;");
+
+  return [
+
+    'data'=>$results,
+    'total'=>(int)$resultTotal[0]["nrtotal"],
+    'pages'=>ceil($resultTotal[0]["nrtotal"] / $itemPerPage)
+  ];
+}
+
 
 
 
